d13c9c60dcff433a12ecc19fcc8fdc7c69c249db
TezadaConnect <terrencecalzada@gmail.com>
2024-04-05 21:23:25 +0800

    Added simple controller class

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,13 @@
-import { Application, Sprite, Assets } from 'pixi.js';
 import './css/main.css';
+import { MainSceneController } from './controllers/main_scene_controller';
+import { Ticker } from 'pixi.js';
 
-const app: Application = new Application();
+const startGame = async () => {
+  const controller: MainSceneController = new MainSceneController();
+  await controller.init({ background: "#7371fc", resizeTo: window});
+  document.body.appendChild(controller.canvas);
+  await controller.ready();
+  Ticker.shared.add(controller.process.bind(controller));
+}
 
-await app.init({ background: "#7371fc", resizeTo: window});
-
-document.body.appendChild(app.canvas);
-
-// Kinda texture2d
-const happyPackedScene = await Assets.load('https://blog.hootsuite.com/wp-content/uploads/2020/02/Image-copyright-556x556.png');
-
-// A node like sprite class
-const image = new Sprite(happyPackedScene);
-image.x = app.renderer.width / 2;
-image.y = app.renderer.height / 2;
-image.scale = 0.5;
-image.anchor.x = 0.5;
-image.anchor.y = 0.5;
-
-// its like add child to the node in godot when manually instantiating a scene
-app.stage.addChild(image);
-
-// this is kinda like the godot process method
-app.ticker.add(() => {
-  image.rotation += .02;
-});
+startGame();
